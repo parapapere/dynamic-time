1d275c7d5014e285e716fbdd7f4a4a6f3de7811d
Adoler420 <1ta.chmielewski@gmail.com>
2023-05-06 13:35:06 +0200

    Copy of Kamper code

---
+++ main.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy.io import wavfile
+import IPython.display as ipyd
+import librosa
+import librosa.display
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.spatial import distance as dist
+
+
+# x_fn = "audio/hello1.wav"
+# x_fn = "audio/hello2.wav"
+x_fn = "audio/goodbye.wav"
+f_s, x = wavfile.read(x_fn)
+ipyd.Audio(rate=f_s, data=x)
+
+# y_fn = "audio/hello3.wav"
+y_fn = "audio/bye.wav"
+f_s, y = wavfile.read(y_fn)
+ipyd.Audio(rate=f_s, data=y)
+
+# Mel-scale spectrogram
+n_fft = int(0.025*f_s)      # 25 ms
+hop_length = int(0.01*f_s)  # 10 ms
+mel_spec_x = librosa.feature.melspectrogram(y=x/1.0, sr=f_s, n_mels=40, n_fft=n_fft, hop_length=hop_length)
+log_mel_spec_x = np.log(mel_spec_x)
+
+# Plot log-Mel spectrogram
+fig, ax = plt.subplots()#figsize=(9, 5))
+ax.imshow(log_mel_spec_x, origin="lower", interpolation="nearest");
+plt.ylabel("Feature dimensions")
+plt.xlabel("Frames");
+
+# Mel-scale spectrogram
+mel_spec_y = librosa.feature.melspectrogram(y=y/1.0, sr=f_s, n_mels=40, n_fft=n_fft, hop_length=hop_length)
+log_mel_spec_y = np.log(mel_spec_y)
+
+# Plot log-Mel spectrogram
+fig, ax = plt.subplots(figsize=(9, 5))
+ax.imshow(log_mel_spec_y, origin="lower", interpolation="nearest");
+plt.ylabel("Feature dimensions")
+plt.xlabel("Frames");
+
+x_seq = log_mel_spec_x.T
+y_seq = log_mel_spec_y.T
+
+dist_mat = dist.cdist(x_seq, y_seq, "cosine")
+path, cost_mat = dp(dist_mat)
+print("Alignment cost: {:.4f}".format(cost_mat[-1, -1]))
+
+fig = plt.figure(figsize=(9, 10))
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(313)
+
+ax1.imshow(log_mel_spec_x, origin="lower", interpolation="nearest")
+ax2.imshow(log_mel_spec_y, origin="lower", interpolation="nearest")
+ax1.axis("off")
+ax2.axis("off")
+
+for x_i, y_j in path:
+    con = ConnectionPatch(
+        xyA=(x_i, 0), xyB=(y_j, log_mel_spec_y.shape[0] - 1), coordsA="data", coordsB="data",
+        axesA=ax1, axesB=ax2, color="C7"
+        )
+    ax2.add_artist(con)
+
+plt.figure(figsize=(9, 5))
+plt.subplot(121)
+plt.title("Distance matrix")
+plt.imshow(dist_mat, cmap=plt.cm.binary, interpolation="nearest", origin="lower")
+plt.subplot(122)
+plt.title("Cost matrix")
+plt.imshow(cost_mat, cmap=plt.cm.binary, interpolation="nearest", origin="lower")
+x_path, y_path = zip(*path)
+plt.plot(y_path, x_path);
